86d919ef990a046902a1438de0426a553719e3fb
dfd1123 <dfd1123@naver.com>
2023-05-09 18:40:59 +0900

    eslint airbnb 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,11 @@
+/* eslint-disable no-undef */
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'airbnb',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
